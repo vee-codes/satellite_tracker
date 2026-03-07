c5cf3c84e261ec56f4e9c68d1ca50e1358c5e4f8
vee-codes <17402407+vee-codes@users.noreply.github.com>
2026-03-07 15:07:31 -0800

    successful api call

--- main.py
+++ main.py
@@ -1,8 +1,15 @@
 import sat_api
+from satellite_ids import * 
+import json
+
+def pretty_print_json(data,indent=2,sort_keys=False):
+     print(json.dumps(data, indent=indent, sort_keys=sort_keys, default=str))
 
 def main():
-    print("Hello from satellite-tracker!")
-    iss = sat_api.satellite_tracker()
+    
+    iss_tracker = sat_api.satellite_tracker()
+    iss_tle = iss_tracker.get_tle(ISS_ID)
+    pretty_print_json(iss_tle)
     
 if __name__ == "__main__":
     main()
--- sat_api.py
+++ sat_api.py
@@ -1,10 +1,24 @@
 import os
+import satellite_ids
+import requests
 from dotenv import load_dotenv
 
 class satellite_tracker:
 
     BASE_URL = "https://api.n2yo.com/rest/v1/satellite/"
     API_KEY = os.getenv("N2YO_API_KEY")
-    print(f"my api key: {API_KEY}")
+
     def __init__(self):
-        pass
+        self.s = requests.Session()
+
+    def get_tle(self,sat_id:int):
+        """Retrieve the Two Line Elements (TLE) for a satellite identified by NORAD id."""
+        #Request: https://api.n2yo.com/rest/v1/satellite/tle/25544&apiKey=API_KEY
+        try:
+            url = f"{self.BASE_URL}tle/{sat_id}/&apiKey={satellite_tracker.API_KEY}"
+            response = self.s.get(url,timeout=20)
+            return response.json()
+    
+        except requests.exceptions.RequestException as e:
+            print(f"Error fetching TLE: {e}")
+            return None
